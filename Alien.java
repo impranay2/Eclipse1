c6c5a5369d1c14cd768ddb97582a5e74d6298cc0
impranay2 <41439897+impranay2@users.noreply.github.com>
2018-07-19 20:36:29 +0530

    Alien added

---
+++ Alien.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class Alien {
+
+	private String aname;
+	private Integer aid;
+	private String lang;
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public Integer getAid() {
+		return aid;
+	}
+	public void setAid(Integer aid) {
+		this.aid = aid;
+	}
+	public String getLang() {
+		return lang;
+	}
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+	
+}
